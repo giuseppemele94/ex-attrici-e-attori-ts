4cd9a008dbb84b8fa6f8e13067465ef22925d983
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-04-29 10:02:32 +0200

    snack-2

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,31 @@ type Person = {
   biography: string,
   image: string,
 }
+
+/* SNACK-2 *
+📌 Milestone 2
+Crea un type alias Actress che oltre a tutte le proprietà di Person,
+ aggiunge le seguenti proprietà:
+
+- most_famous_movies: una tuple di 3 stringhe
+- awards: una stringa
+- nationality: una stringa tra un insieme definito di valori.
+Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, 
+French, Indian, Israeli, Spanish, South Korean, Chinese. */
+
+type Actress = Person & {
+  most_famous_movies: [string,string,string],
+  awards: string ; 
+  nationality: 
+  | "American" 
+  | "British" 
+  | "Australian" 
+  | "Israeli-American" 
+  | "South African" 
+  | "French" 
+  | "Indian"
+  | "Israeli" 
+  | "Spanish" 
+  | "South Korean" 
+  | "Chinese"
+}
