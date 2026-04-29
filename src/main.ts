f2ddc9216a878bfd0e7435f452e0cbcf678dbebf
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-04-29 10:37:27 +0200

    milestone-3

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ Il tipo deve includere le seguenti proprietà:
 
 type Person = {
   readonly id: number,
-  readonly name : string, 
+  readonly name: string,
   birth_year: number,
   death_year?: number,
   biography: string,
@@ -32,18 +32,73 @@ Le nazionalità accettate sono: American, British, Australian, Israeli-American,
 French, Indian, Israeli, Spanish, South Korean, Chinese. */
 
 type Actress = Person & {
-  most_famous_movies: [string,string,string],
-  awards: string ; 
-  nationality: 
-  | "American" 
-  | "British" 
-  | "Australian" 
-  | "Israeli-American" 
-  | "South African" 
-  | "French" 
+  most_famous_movies: [string, string, string],
+  awards: string;
+  nationality:
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
   | "Indian"
-  | "Israeli" 
-  | "Spanish" 
-  | "South Korean" 
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
   | "Chinese"
+}
+
+/* SNACK-3*
+
+Crea una funzione getActress che, dato un id, effettua una chiamata a:
+
+GET /actresses/:id
+La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+
+Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta. */
+
+function isActress(data: unknown): data is Actress {
+  if (typeof data !== "object" || data === null) {
+    return false;
+  }
+
+  return (
+
+    "id" in data && typeof data.id === "number" &&
+    "name" in data && typeof data.name === "string" &&
+    "birth_year" in data && typeof data.birth_year === "number" &&
+    (!("death_year" in data) || typeof data.death_year === "number") &&
+    "biography" in data && typeof data.biography === "string" &&
+    "image" in data && typeof data.image === "string" &&
+    "most_famous_movies" in data && Array.isArray(data.most_famous_movies) && data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every(movie => typeof movie === "string") &&
+    "awards" in data && typeof data.awards === "string" &&
+    "nationality" in data && typeof data.nationality === "string"
+
+  );
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    if (!response.ok) {
+      return null;
+    }
+    //se la risposta è andata a buon fine raccolgo il risultato
+    const data: unknown = await response.json();
+
+    if (!isActress(data)) {
+      throw new Error("Formato dati non valido");
+    }
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore durante il recupero dei dati", error);
+    } else {
+      console.error("Errore sconosciuto", error);
+    }
+    return null;
+
+  }
 }
